30a3d2a342171f571fdfabb95243aa3b8b4494ac
jarruda <joao.arruda@booking.com>
2023-09-15 17:00:58 +0200

    chore: get repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY ?? '/',
+  base: process.env.GITHUB_REPOSITORY_NAME ?? '/',
   plugins: [
     vue(),
   ],
