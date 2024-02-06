0fc6c4bb524a145c54637c14095851d59cd9f6cb
jarruda <joao.arruda@booking.com>
2024-02-06 17:46:13 +0100

    fix: use hash history

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Burger from '@/views/Burger.vue'
 import Pixel from '@/views/Pixel.vue'
 import MapSetup from '@/views/MapSetup.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
